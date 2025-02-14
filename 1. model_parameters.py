54b40798832bf171d317e7c35a5d8f63777ed198
Natalia Czyżyk <87877665+natalia-czyzyk@users.noreply.github.com>
2025-02-14 14:25:37 +0100

    Update and rename model parameters.py to 1. model_parameters.py

--- 1. model_parameters.py
+++ 1. model_parameters.py
@@ -8,7 +8,7 @@ df = pd.read_csv('survey.csv')
 df = df.drop(columns = ['Sygnatura czasowa'])
 df_num = df
 
-# Numerical dataframe for correlations
+# Explanatory data analysis + dataframe
 
 df_num.columns = ['D1', 'D2', 'D3', 'D4', 'D5', 'S1', 'A1', 'F1', 'F2', 'F3', 'F4', 'F5', 'MC1', 'MC2', 'MC3', 'PI1', 'PI2', 'PI3', 'SI1', 'SI2', 'SI3', 'IB1', 'EA1', 'EA2', 'EA3', 'R1']
 df_num['D1'] = df_num['D1'].replace(['kobieta', 'mężczyzna', 'inne/nie chcę podawać'], [1, 3, 2])
@@ -22,11 +22,10 @@ df_num = df_num.replace(['bardzo ważne', 'ważne', 'średnio ważne', 'trochę 
 df_num = df_num.replace(['zdecydowanie się zgadzam', 'raczej się zgadzam', 'nie mam zdania/trudno powiedzieć', 'raczej się nie zgadzam', 'zdecydowanie się nie zgadzam'], [5, 4, 3, 2, 1])
 #df_num = df_num.replace(['zbyt wysoka cena', 'niska dostępność w sklepach stacjonarnych', 'brak dostrzegalnej różnicy w porównaniu z konwencjonalnymi kosmetykami', 'krótki okres przydatności do użycia', 'niski stopień wiedzy na temat kosmetyków zrównoważonych'], [1, 2, 3, 4, 5])
 
-# Columns to compare
-
 columns_names = ['D1', 'D2', 'D3', 'D4', 'D5', 'S1', 'F1', 'F2', 'F3', 'F4', 'F5', 'MC1', 'MC2', 'MC3', 'PI1', 'PI2', 'PI3', 'SI1', 'SI2', 'SI3', 'IB1', 'EA1', 'EA2', 'EA3']
 
-# srednia pytan MC, PI, SI, EA
+
+# Average of questions MC, PI, SI, EA
     
 df_num['SI1'] = df_num['SI1'].replace([1, 2, 3, 4, 5], [-5, -4, -3, -2, -1])
 df_num['SI2'] = df_num['SI2'].replace([1, 2, 3, 4, 5], [-5, -4, -3, -2, -1])
@@ -38,14 +37,13 @@ for var in ['MC', 'PI', 'EA', 'SI']:
     df_num.insert(var_last_index + 1, var, mean_values)
 
 df_num = df_num.append(df_num.mean(), ignore_index=True)
-    
-# Parameters:
-# each agents': PI, SI, EA, MC? should MC be per agent or different MC for 3 types of extrenal factors
+
+
+# Parameters PI (power to influence) and SI (susceptibility to be influenced)
 
 df2 = df_num[['PI', 'SI']]
 df2 = df2.dropna()
 df2['status'] = df2[['SI', 'PI']].sum(axis=1).round()
-
 
 status_counts = df2['status'].value_counts()
 
